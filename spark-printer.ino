e19b8a4e6a6b5b630c93c08665cd34f07231fa1f
Henry Stanley <henry@henrystanley.com>
2014-10-14 17:07:01 +0100

    updated code to publish uptime every 15sec, and accept print requests

--- spark-printer.ino
+++ spark-printer.ino
@@ -1,32 +1,41 @@
+// This #include statement was automatically added by the Spark IDE.
+// #include "lib1.h"
+
 // enables second UART serial on pin D1(TX) and D0(RX)
-#include "Serial2/Serial2.h"
+// #include "Serial2/Serial2.h"
 
 /* Include the RFID library */
 /* SEE RFID.h for selecting Hardware or Software SPI modes */
-#include "RFID.h"
+// #include "RFID.h"
 
 /* Define the pins used for the SS (SDA) and RST (reset) pins for BOTH hardware and software SPI */
 /* Change as required */
-#define SS_PIN      A2      // Same pin used as hardware SPI (SS)
-#define RST_PIN     D2
+// #define SS_PIN      A2      // Same pin used as hardware SPI (SS)
+// #define RST_PIN     D2
 
 /* Define the pins used for the DATA OUT (MOSI), DATA IN (MISO) and CLOCK (SCK) pins for SOFTWARE SPI ONLY */
 /* Change as required and may be same as hardware SPI as listed in comments */
-#define MOSI_PIN    A5      // hardware SPI: A5
-#define MISO_PIN    A4      //     "     " : A4
-#define SCK_PIN     A3      //     "     " : A3
+// #define MOSI_PIN    A5      // hardware SPI: A5
+// #define MISO_PIN    A4      //     "     " : A4
+// #define SCK_PIN     A3      //     "     " : A3
 
 // This #include statement was automatically added by the Spark IDE.
-#include "Adafruit_Thermal/Adafruit_Thermal.h"
+#include "Adafruit_Thermal.h"
 
 /* Create an instance of the RFID library */
-#if defined(_USE_SOFT_SPI)
-  RFID(int chipSelectPin, int NRSTPD, uint8_t mosiPin, uint8_t misoPin, uint8_t clockPin);    // Software SPI
-#else
-  RFID RC522(SS_PIN, RST_PIN);    // Hardware SPI
-#endif
+// #if defined(_USE_SOFT_SPI)
+//   RFID(int chipSelectPin, int NRSTPD, uint8_t mosiPin, uint8_t misoPin, uint8_t clockPin);    // Software SPI
+// #else
+//   RFID RC522(SS_PIN, RST_PIN);    // Hardware SPI
+// #endif
 
-Adafruit_Thermal printer;
+int printer_RX_Pin = 0;  // Serial1 Rx pin on Spark to the green wire on printer
+int printer_TX_Pin = 0;  // Serial1 Tx pin on Spark to the yellow wire on printer
+
+unsigned long lastTime = 0UL;
+char publishString[40];
+
+Adafruit_Thermal printer(printer_RX_Pin, printer_TX_Pin);
 
 // master print command
 int print(String args){
@@ -48,6 +57,13 @@ int print(String args){
     printALine(bodyText);
     return 1;
   }
+  else if(command == "HELO") {
+    printALine(bodyText);
+    // printer.wake();
+    // printer.feed(2);
+    // printer.sleep();
+    return 3;
+  }
   else {
     return -1;
   }
@@ -63,7 +79,7 @@ int printImage(String args){
 int printALine(String args){
    printer.wake();
    printer.println(args);
-   Serial.println(args); // print the line to the USB serial port for debugging
+   printer.feed(2);
    printer.sleep();
    return 1;
 }
@@ -72,8 +88,8 @@ int printBold(String args){
   printer.wake();
   printer.boldOn();
   printer.println(args);
-  Serial.println(args); // print the line to the USB serial port for debugging
   printer.boldOff();
+  printer.feed(2);
   printer.sleep();
   return 1;
 }
@@ -83,69 +99,35 @@ void setup(){
     
   //
   // RFID SETUP
-  Serial.begin(9600);
+//   Serial.begin(9600);
   
   /* Enable the SPI interface */
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setClockDivider(SPI_CLOCK_DIV8);
-  SPI.begin();
+//   SPI.setDataMode(SPI_MODE0);
+//   SPI.setBitOrder(MSBFIRST);
+//   SPI.setClockDivider(SPI_CLOCK_DIV8);
+//   SPI.begin();
   
   /* Initialise the RFID reader */
-  RC522.init();
+//   RC522.init();
 
+//   Serial.begin(9600);
+  
   Serial1.begin(19200);
-  printer.begin(&Serial1);
+  printer.begin();
   
   Spark.function("print", print);
-  Spark.function("printImage", printImage);
+//   Spark.function("printImage", printImage);
 
-
-  //
-  // PRINTER TEST PAGE
-
-  // Test inverse on & off
-  printer.inverseOn();
-  printer.println("Inverse ON");
-  printer.inverseOff();
-
-  // Test character double-height on & off
-  printer.doubleHeightOn();
-  printer.println("Double Height ON");
-  printer.doubleHeightOff();
-
-  // Set text justification (right, center, left) -- accepts 'L', 'C', 'R'
-  printer.justify('R');
-  printer.println("Right justified");
-  printer.justify('C');
-  printer.println("Center justified");
-  printer.justify('L');
-  printer.println("Left justified");
-
-  // Test more styles
+  printer.println("Team Spark rulez!");
+  printer.feed(1);
   printer.boldOn();
-  printer.println("Bold text");
+  printer.println("Spark Printer is online.");
   printer.boldOff();
-
-  printer.underlineOn(); 
-  printer.println("Underlined text ");
-  printer.underlineOff();
-
-  printer.setSize('L');     // Set type size, accepts 'S', 'M', 'L'
-  printer.println("Large"); // Print line
-  printer.setSize('M');
-  printer.println("Medium");
-  printer.setSize('S');
-  printer.println("Small");
-
-  printer.justify('C');
-  printer.println("normal\nline\nspacing");
-  printer.setLineHeight(50);
-  printer.println("Taller\nline\nspacing");
-  printer.setLineHeight(); // Reset to default
-  printer.justify('L');
-
-  printer.println("Adafruit!");
+  printer.feed(1);
+  printer.println("---------------");
+  printer.feed(1);
+  printer.feed(1);
+  printer.feed(1);
   printer.feed(1);
 
   printer.sleep();      // Tell printer to sleep
@@ -155,27 +137,40 @@ void setup(){
 
 void loop()
 {
-  /* Temporary loop counter */
-  uint8_t i;
+//   /* Temporary loop counter */
+//   uint8_t i;
 
-  /* Has a card been detected? */
-  if (RC522.isCard())
-  {
-    /* If so then get its serial number */
-    RC522.readCardSerial();
+//   /* Has a card been detected? */
+//   if (RC522.isCard())
+//   {
+//     /* If so then get its serial number */
+//     RC522.readCardSerial();
+//     // tone(A2, 1174.66, 500);
+//     Serial.println("Card detected:");
 
-    Serial.println("Card detected:");
-
-    /* Output the serial number to the UART */
-    for(i = 0; i <= 4; i++)
-    {
-      Serial.print(RC522.serNum[i],HEX);
-      Serial.print(" ");
-    }
-    Serial.println();
-  }
-  else
-      Serial.println("Card NOT detected:");
+//     /* Output the serial number to the UART */
+//     for(i = 0; i <= 4; i++)
+//     {
+//       Serial.print(RC522.serNum[i],HEX);
+//       Serial.print(" ");
+//     }
+//     Serial.println();
+//   }
+//   else
+//       Serial.println("Card NOT detected:");
       
-  delay(1000);
+//   delay(1000);
+
+    unsigned long now = millis();
+    //Every 15 seconds publish uptime
+    if (now-lastTime>15000UL) {
+        lastTime = now;
+        // now is in milliseconds
+        unsigned nowSec = now/1000UL;
+        unsigned sec = nowSec%60;
+        unsigned min = (nowSec%3600)/60;
+        unsigned hours = (nowSec%86400)/3600;
+        sprintf(publishString,"%u:%u:%u",hours,min,sec);
+        Spark.publish("Uptime",publishString);
+    }
 }
