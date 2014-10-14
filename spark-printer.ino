e25fcdd4df6b52dcf307cca5d9ca531cf4c0f333
Henry Stanley <henry@henrystanley.com>
2014-10-14 19:08:46 +0100

    main program file with RFID methods in place, but actual void() loop commented out for now

--- spark-printer.ino
+++ spark-printer.ino
@@ -1,39 +1,37 @@
-// This #include statement was automatically added by the Spark IDE.
-// #include "lib1.h"
-
 // enables second UART serial on pin D1(TX) and D0(RX)
 // #include "Serial2/Serial2.h"
 
 /* Include the RFID library */
 /* SEE RFID.h for selecting Hardware or Software SPI modes */
-// #include "RFID.h"
+#include "RFID.h"
 
 /* Define the pins used for the SS (SDA) and RST (reset) pins for BOTH hardware and software SPI */
 /* Change as required */
-// #define SS_PIN      A2      // Same pin used as hardware SPI (SS)
-// #define RST_PIN     D2
+#define SS_PIN      A2      // Same pin used as hardware SPI (SS)
+#define RST_PIN     D2
 
 /* Define the pins used for the DATA OUT (MOSI), DATA IN (MISO) and CLOCK (SCK) pins for SOFTWARE SPI ONLY */
 /* Change as required and may be same as hardware SPI as listed in comments */
-// #define MOSI_PIN    A5      // hardware SPI: A5
-// #define MISO_PIN    A4      //     "     " : A4
-// #define SCK_PIN     A3      //     "     " : A3
+#define MOSI_PIN    A5      // hardware SPI: A5
+#define MISO_PIN    A4      //     "     " : A4
+#define SCK_PIN     A3      //     "     " : A3
 
 // This #include statement was automatically added by the Spark IDE.
 #include "Adafruit_Thermal.h"
 
 /* Create an instance of the RFID library */
-// #if defined(_USE_SOFT_SPI)
-//   RFID(int chipSelectPin, int NRSTPD, uint8_t mosiPin, uint8_t misoPin, uint8_t clockPin);    // Software SPI
-// #else
-//   RFID RC522(SS_PIN, RST_PIN);    // Hardware SPI
-// #endif
+#if defined(_USE_SOFT_SPI)
+  RFID(int chipSelectPin, int NRSTPD, uint8_t mosiPin, uint8_t misoPin, uint8_t clockPin);    // Software SPI
+#else
+  RFID RC522(SS_PIN, RST_PIN);    // Hardware SPI
+#endif
 
 int printer_RX_Pin = 0;  // Serial1 Rx pin on Spark to the green wire on printer
 int printer_TX_Pin = 0;  // Serial1 Tx pin on Spark to the yellow wire on printer
 
 unsigned long lastTime = 0UL;
 char publishString[40];
+char scannedCardID[10];
 
 Adafruit_Thermal printer(printer_RX_Pin, printer_TX_Pin);
 
@@ -98,17 +96,17 @@ int printBold(String args){
 void setup(){
     
   //
-  // RFID SETUP
-//   Serial.begin(9600);
+//   RFID SETUP
+   Serial.begin(9600);
   
   /* Enable the SPI interface */
-//   SPI.setDataMode(SPI_MODE0);
-//   SPI.setBitOrder(MSBFIRST);
-//   SPI.setClockDivider(SPI_CLOCK_DIV8);
-//   SPI.begin();
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
+  SPI.begin();
   
   /* Initialise the RFID reader */
-//   RC522.init();
+  RC522.init();
 
 //   Serial.begin(9600);
   
@@ -147,19 +145,26 @@ void loop()
 //     RC522.readCardSerial();
 //     // tone(A2, 1174.66, 500);
 //     Serial.println("Card detected:");
-
+    
 //     /* Output the serial number to the UART */
 //     for(i = 0; i <= 4; i++)
 //     {
 //       Serial.print(RC522.serNum[i],HEX);
-//       Serial.print(" ");
+//       scannedCardID + (RC522.serNum[i]);
 //     }
 //     Serial.println();
+    
+//     Spark.publish("scannedCardID",scannedCardID);
+//     scannedCardID == "";
 //   }
 //   else
+//   {
 //       Serial.println("Card NOT detected:");
-      
-//   delay(1000);
+//       scannedCardID == "no card";
+//       Spark.publish("scannedCardID",scannedCardID);
+//       scannedCardID == "";
+//   }   
+//   delay(500);
 
     unsigned long now = millis();
     //Every 15 seconds publish uptime
